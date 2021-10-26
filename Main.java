455dabacefbc723bd664ec36e2025b10de6bd7a4
AndryRusff <jam.track@yandex.ru>
2021-10-26 20:50:15 +0300

    init

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int current = 2000000000;
+        int transact = 500000000;
+        int after = current + transact;
+        System.out.println(after);
+    }
+}
